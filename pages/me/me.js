21896ce04eca8f133ed8cd8b564275d50c69cfe1
wardenger <wardenger@gmail.com>
2016-12-15 17:48:37 +0800

    update README

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,36 +1,22 @@
-//index.js
-//获取应用实例
-
-
 var util = require('../../utils/util.js')
 var Api = require('../../utils/api.js')
-
-
 
 Page({
   data: {
     token: '',
-    motto: 'Hello World',
     userInfo: '',
     list: [],
     page: 1,
     per: 5,
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
 
   onLoad: function () {
-    var _this = this
     wx.getStorage({
       key: 'info',
-      success: function(res){
-        _this.setData({userInfo: JSON.parse(res.data).wxInfo})
-        _this.setData({token: JSON.parse(res.data).token})
-        _this.fetchList()
+      success: (res) => {
+        this.setData({userInfo: JSON.parse(res.data).wxInfo})
+        this.setData({token: JSON.parse(res.data).token})
+        this.fetchList()
       }
     })
   },
@@ -67,7 +53,7 @@ Page({
   
   deleteHandle: function(event) {
     console.log(event.currentTarget.dataset.id)
-    console.log('delete')
+    var _this = this
     wx.showModal({
       title: '删除',
       content: '确定删除这个资源？',
@@ -75,7 +61,7 @@ Page({
         if(res.confirm) {
           console.log('用户点击确定')
           wx.request({
-            url: 'https://tinyapp.sparklog.com/imagination/'+ event.currentTarget.dataset.id+'?token='+token,
+            url: 'https://tinyapp.sparklog.com/imagination/'+ event.currentTarget.dataset.id+'?token='+_this.data.token,
             method: 'DELETE',
             success: function(res){
               console.log('删除成功')
